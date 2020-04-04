0391a0a7179a763ceb8ed0ad1dcc9f29f87443b2
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-04 16:28:10 -0300

    raphael: Build livedisplay 2.0 hal

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_raphael
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
